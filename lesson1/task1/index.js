8cc9a0801d64abd5f1935272b52d3f6a13a5cfb1
DenisShylov <dodjer123@gmail.com>
2022-07-27 15:33:41 +0300

    lesson1/task1

---
+++ lesson1/task1/index.js
@@ -0,0 +1,9 @@
+const rootElem = document.querySelector('#root');
+
+const divElem = React.createElement(
+  'div',
+  { className: 'greeting' },
+  'Hello, React!'
+);
+
+ReactDOM.render(divElem, rootElem);
