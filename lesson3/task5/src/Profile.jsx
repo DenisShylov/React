bdc1e424ff3a964f612ee68193d16373ce09cb02
DenisShylov <dodjer123@gmail.com>
2022-07-28 23:52:43 +0300

    fix

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './index.scss';
+import moment from 'moment';
 
 const Profile = (userData) => {
-  const age = Math.floor(
-    (new Date().getTime() - new Date(userData.birthDate)) /
-      (24 * 3600 * 365 * 1000)
-  );
+  const date = moment(userData.birthDate).format('MMM Do YY');
   return (
     <div className="profile">
-      {`My name is ${userData.firstName} ${userData.lastName}. I'm ${age} years old and I was born in ${userData.birthPlace}`}
+      <div className="profile__name">{`${userData.firstName} ${userData.lastName}`}</div>
+      <div className="profile__birth">{`Was born ${date} in ${userData.birthPlace}`}</div>
     </div>
   );
 };
