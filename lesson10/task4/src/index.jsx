78c1de0ce68252e539c730aa9363debaa9625ccc
DenisShylov <dodjer123@gmail.com>
2022-09-07 16:34:48 +0300

    upd

--- lesson10/task4/src/index.jsx
+++ lesson10/task4/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+
 import FilterableProductTable from './FilterableProductTable';
 
 const rootElem = document.querySelector('#root');
