447e29d8a986986a0f9b562ef5e2d61ff9fd076e
DenisShylov <dodjer123@gmail.com>
2022-09-26 21:37:32 +0300

    task2

---
+++ lesson13/task2/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import Contacts from './Contacts';
+import PageNotFound from './PageNotFound';
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="page">
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Route>
+              <Link to="/">Home</Link>
+            </Route>
+          </li>
+          <li className="navigation__item">
+            <Route>
+              <Link to="/products">Products</Link>
+            </Route>
+          </li>
+        </ul>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products" component={Products}></Route>
+      </div>
+    </BrowserRouter>
+  );
+};
+export default App;
